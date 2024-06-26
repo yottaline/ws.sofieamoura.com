d83f16552caa234abd287043d032f123699f8ed5
Ahmed-elnoman <91709806+Ahmed-elnoman@users.noreply.github.com>
2024-06-27 00:35:27 +0200

    forget

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -35,7 +35,7 @@ class PasswordResetLinkController extends Controller
         $status = Password::sendResetLink(
             $request->only('email')
         );
-
+return $status;
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
